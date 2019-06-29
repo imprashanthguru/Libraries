62ac3ca88dc8138d69e323e67bb2dde5ca25907d
Prashanth Gurunath <prashanthguru.17@gmail.com>
2019-06-29 09:15:21 +0530

    Initial commit

---
+++ mytoasterlibrary/src/main/java/com/prashanthgurunath/mytoasterlibrary/ToasterMessage.java
@@ -0,0 +1,10 @@
+package com.prashanthgurunath.mytoasterlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+    public static void s(Context c, String message) {
+        Toast.makeText(c, message,Toast.LENGTH_LONG).show();
+    }
+}
